0d24e2432cabb6adace0ac2d6d72e79558ce7789
Nikil rajan <115686502+Nikilraja@users.noreply.github.com>
2023-02-28 01:18:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Write a “person” class to hold all the details.
+
+let Person ={
+    name:"Nikil",
+    age:"25",
+    fatherName:"sivasankar",
+    adress:"299,melai street, anathur",
+    education:"bca",
+    nationality:"indian",
+    extraActivity:"NSS,NCC",
+    getPersonDetails:
+
+
+}
